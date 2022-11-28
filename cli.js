3f27935c98fdd771fb0b726d8f7f132999fba48c
KartoffelToby <kontakt@t-haber.de>
2022-11-28 22:03:54 +0100

    [TASk] fix path

--- cli.js
+++ cli.js
@@ -6,5 +6,5 @@ const integration = process.env.INTEGRATION || 'better_thermostat';
     const custom_integrations = await data.json();
     const badgeData = await fetch(`https://img.shields.io/badge/Active_installations-${custom_integrations[integration].total}-2ea44f?style=for-the-badge`);
     const badge = await badgeData.text();
-    fs.writeFileSync('badge.svg', badge);
+    fs.writeFileSync('dist/badge.svg', badge);
 })();
