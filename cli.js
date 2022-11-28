31dd3c5a9350a8ed3dee4d7d8ae9e4de42865a27
KartoffelToby <kontakt@t-haber.de>
2022-11-28 22:01:00 +0100

    [TASK] add cli

---
+++ cli.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const integration = process.env.INTEGRATION || 'better_thermostat';
+
+(async() => {
+    const data = await fetch('https://analytics.home-assistant.io/custom_integrations.json');
+    const custom_integrations = await data.json();
+    const badgeData = await fetch(`https://img.shields.io/badge/Active_installations-${custom_integrations[integration].total}-2ea44f?style=for-the-badge`);
+    const badge = await badgeData.text();
+    fs.writeFileSync('badge.svg', badge);
+})();
